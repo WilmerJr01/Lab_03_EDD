4cfca8f5e694d279d2839d7898ad14457e2d641a
WilmerJr01 <jwsantiago@uninorte.edu.co>
2023-11-06 13:24:53 -0500

    ReDiseño de Niveles

--- Lab03/src/lab03/NivelesM.java
+++ Lab03/src/lab03/NivelesM.java
@@ -1,26 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package lab03;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 
-/**
- *
- * @author WILME
- */
 public class NivelesM extends javax.swing.JFrame {
 
-    /**
-     * Creates new form VentanaPrincipal
-     */
+    private Timer timer;
+    public int progress = 0;
+
     public NivelesM() {
         initComponents();
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setTitle("Pregunta");
         this.setIconImage(new ImageIcon(getClass().getResource("Imagenes/LogoDM.png")).getImage());
+        timer = new Timer(1000, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (progress >= 90) {
+                    timer.stop();
+                    new PantallaFinal().setVisible(true);
+                    dispose();
+                } else {
+                    progress++;
+                    BarraDeTiempo.setValue(progress);
+                    String MostrarTiempo;
+                    MostrarTiempo = String.valueOf(progress);
+                    TiempoLbl.setText(MostrarTiempo);
+                }
+            }
+        });
+        timer.start();
     }
 
     /**
@@ -33,6 +46,7 @@ public class NivelesM extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        TiempoLbl1 = new javax.swing.JLabel();
         NumeroDeNivel = new javax.swing.JLabel();
         TiempoLbl = new javax.swing.JLabel();
         MarcadoresBtn = new javax.swing.JButton();
@@ -52,47 +66,52 @@ public class NivelesM extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(253, 184, 19));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        TiempoLbl1.setFont(new java.awt.Font("Malgun Gothic", 1, 24)); // NOI18N
+        TiempoLbl1.setForeground(new java.awt.Color(255, 255, 255));
+        TiempoLbl1.setText("Tiempo:");
+        jPanel1.add(TiempoLbl1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 50, -1, -1));
+
         NumeroDeNivel.setFont(new java.awt.Font("Malgun Gothic", 1, 36)); // NOI18N
         NumeroDeNivel.setForeground(new java.awt.Color(255, 255, 255));
         NumeroDeNivel.setText("00");
-        jPanel1.add(NumeroDeNivel, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 40, 120, 90));
+        jPanel1.add(NumeroDeNivel, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 70, 120, 90));
 
         TiempoLbl.setFont(new java.awt.Font("Malgun Gothic", 1, 24)); // NOI18N
-        TiempoLbl.setText("00:00");
-        jPanel1.add(TiempoLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 0, -1, -1));
+        TiempoLbl.setForeground(new java.awt.Color(255, 255, 255));
+        TiempoLbl.setText("0");
+        jPanel1.add(TiempoLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 50, -1, -1));
 
         MarcadoresBtn.setBackground(new java.awt.Color(253, 184, 19));
         MarcadoresBtn.setFont(new java.awt.Font("Malgun Gothic", 1, 24)); // NOI18N
         MarcadoresBtn.setForeground(new java.awt.Color(255, 255, 255));
         MarcadoresBtn.setText("Comprobar");
         MarcadoresBtn.setBorderPainted(false);
-        jPanel1.add(MarcadoresBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 370, -1, -1));
+        MarcadoresBtn.setDefaultCapable(false);
+        jPanel1.add(MarcadoresBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 410, -1, -1));
 
         BarraDeTiempo.setBackground(new java.awt.Color(255, 255, 255));
-        BarraDeTiempo.setForeground(new java.awt.Color(254, 222, 1));
+        BarraDeTiempo.setForeground(new java.awt.Color(1, 173, 240));
         BarraDeTiempo.setMaximum(90);
         jPanel1.add(BarraDeTiempo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 730, 40));
 
         Titulo.setFont(new java.awt.Font("Malgun Gothic", 1, 36)); // NOI18N
         Titulo.setForeground(new java.awt.Color(255, 255, 255));
         Titulo.setText("Pregunta #");
-        jPanel1.add(Titulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 60, -1, -1));
+        jPanel1.add(Titulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 90, -1, -1));
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        PreguntaLbl.setBackground(new java.awt.Color(255, 255, 255));
         PreguntaLbl.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
         PreguntaLbl.setForeground(new java.awt.Color(253, 184, 19));
         PreguntaLbl.setText("x=3+45-5");
         jPanel2.add(PreguntaLbl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 420, 130));
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 130, 420, 130));
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 180, 420, 130));
 
         jLabel1.setFont(new java.awt.Font("Malgun Gothic", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("X=");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 310, -1, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 350, -1, -1));
 
         jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
@@ -104,7 +123,7 @@ public class NivelesM extends javax.swing.JFrame {
         jTextArea1.setToolTipText("");
         jScrollPane1.setViewportView(jTextArea1);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 310, 230, 40));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 350, 230, 40));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 730, 500));
 
@@ -144,6 +163,14 @@ public class NivelesM extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -159,6 +186,7 @@ public class NivelesM extends javax.swing.JFrame {
     private javax.swing.JLabel NumeroDeNivel;
     private javax.swing.JLabel PreguntaLbl;
     private javax.swing.JLabel TiempoLbl;
+    private javax.swing.JLabel TiempoLbl1;
     private javax.swing.JLabel Titulo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
